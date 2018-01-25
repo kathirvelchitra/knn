81ecf69f720f80cde7220fd03f15fc11290fdfee
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-01-25 15:54:16 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int char[i];
+  string str="keeruma is a cute girl"
+  printf("enter the character");
+  scanf("%d",&char[i]);
+  string strlen="keeruma is a cute girl";
+  scanf("%s",&str);
+  return 0;
+}
+
+

e77baa356da9b706da31df3835c88b7a822949b0
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-01-25 16:52:02 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int g,i,n,m,b;
+	printf("enter the number");
+	scanf("%d",&n);
+	g=0;
+	b=1;
+	m=0;
+	for(i=0;i<n;i++)
+	{
+    printf("%d\n",m);
+    g=b;
+    b=m;
+    m=g+b;
+ 	}
+}

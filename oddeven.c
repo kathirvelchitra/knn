5f14a0f27ee172f1274531c36b0629a949bdec72
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-13 11:07:46 +0000

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\n Enter the a and b values:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    if(c%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
+

6c2d010aec75b7a6b486d2d88a6e56c1b21963a6
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-03-13 11:03:44 +0000

    Create intrest.c

---
+++ intrest.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+void main()
+{
+    int p,n,r,s;
+    printf("enter the values");
+    scanf("%d%d%d",&p,&n,&r);
+    s=(p*n*r)/100;
+    printf("the simple intrest is %d",s);
+}
